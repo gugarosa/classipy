d9261cbd10aeb0afa9062a5c926e7a424d06c7d2
Gustavo Rosa <gth.rosa@uol.com.br>
2017-05-09 10:36:24 -0400

    Prediction view.

--- server.js
+++ server.js
@@ -80,7 +80,10 @@ app.get('/predict/:identifier', function(req, res) {
 	predict.newPrediction(req.params.identifier, function (err, result) {
 		if (err) throw err;
 		console.log(result);
-		res.render('prediction', result);
+    var output = result;
+		res.render('prediction', {
+      title: output
+    });
 	});
 });
 
